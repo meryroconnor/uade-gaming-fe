9810dfc96e7e2828fc8475e4ebb2916f63417f6c
mrivastn <maria.rivas@tiendanube.com>
2024-10-14 21:24:41 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Hero from './components/hompage/Hero';
 import SearchBar from './components/hompage/SearchBar';
 import NavigationIcons from './components/hompage/NavigationIcons';
@@ -22,6 +23,7 @@ function App() {
           <Carousel />
           <ImagesPlaceholder />
         </div>
+        <Footer />
       </div>
     </Router>
   );
