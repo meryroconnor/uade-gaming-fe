95641f5201bf3aabbaf89481968bbf336517a677
mrivastn <maria.rivas@tiendanube.com>
2024-10-21 20:47:56 -0300

    profile init

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import Testimonials from './components/homepage/Testimonials';
 import ProductHero from './components/product_detail/ProductHero';
 import Rating from './components/product_detail/Rating';
 import ProductDetail from './components/product_detail/ProductDetail';
+import UserCover from './components/profile/UserCover';
+import Wishlist from './components/profile/Wishlist';
 
 function App() {
   return (
@@ -20,7 +22,11 @@ function App() {
       <div className="App">
         <Navbar />
         <div className="content-wrapper">
-          <ProductHero />
+          <UserCover />
+          <Wishlist />
+          {/* <ProductHero />
+          <ProductDetail />
+          <Rating rating={2.5} reviewCount={116} /> */}
           {/* <Hero />
           <div className="search-nav-wrapper">
             <SearchBar />
@@ -30,8 +36,7 @@ function App() {
           <Highlights />
           <ImagesPlaceholder />
            */}
-          <ProductDetail />
-          <Rating rating={2.5} reviewCount={116} />
+          
         </div>
         <Footer />
       </div>
