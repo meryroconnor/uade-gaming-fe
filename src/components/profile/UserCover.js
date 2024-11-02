f19e97c9d6ba1d7b3af668393a0ab66d98a8a58e
FNogueira2023 <f.nogueira2022@outlook.com>
2024-11-02 15:29:22 -0300

    falta el purchases/games

--- src/components/profile/UserCover.js
+++ src/components/profile/UserCover.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './UserCover.css';
 
-import image from '../../images/default_user.jpg'
-const UserCover = () => {
+import image from '../../images/default_user.jpg';
+
+const UserCover = ({ profile }) => {
   const [imageUrl, setImageUrl] = useState(image);
-  const [detail] = useState({
-    user: "Polyphony Digital Inc.",
-    userDescription: "An internal Japanese first-party video game development studio for PlayStation Studios. \nBest known for the Gran Turismo racing game series, led by Kazunori Yamauchi.",
+  const [detail, setDetail] = useState({
+    user: "Loading...",
+    userDescription: "",
   });
+
+  const birthDate = profile.birthDate ? new Date(profile.birthDate).toISOString().split('T')[0] : 'Date not available';
 
   const handleImageChange = (event) => {
     const file = event.target.files[0];
@@ -20,41 +23,47 @@ const UserCover = () => {
     }
   };
 
+
   return (
     <div className="cover-container">
-
-        <div className="image-container">
-          <img src={imageUrl} alt="User Logo" className="profile-image" />
-          <div className="button-container">
-            <label htmlFor="logo-upload" className="upload-button">
-              <svg 
-                viewBox="0 0 24 24" 
-                className="camera-icon"
-                stroke="currentColor" 
-                fill="none" 
-                strokeWidth="2" 
-                strokeLinecap="round" 
-                strokeLinejoin="round"
-              >
-                <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
-                <circle cx="12" cy="13" r="4" />
-              </svg>
-              <input
-                id="logo-upload"
-                type="file"
-                className="hidden-input"
-                accept="image/*"
-                onChange={handleImageChange}
-              />
-            </label>
-          </div>
+      <div className="image-container">
+        <img src={imageUrl} alt="User Logo" className="profile-image" />
+        <div className="button-container">
+          <label htmlFor="logo-upload" className="upload-button">
+            <svg
+              viewBox="0 0 24 24"
+              className="camera-icon"
+              stroke="currentColor"
+              fill="none"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
+              <circle cx="12" cy="13" r="4" />
+            </svg>
+            <input
+              id="logo-upload"
+              type="file"
+              className="hidden-input"
+              accept="image/*"
+              onChange={handleImageChange}
+            />
+          </label>
         </div>
-        
-        <div className="text-container">
-          <h1 className="title">{detail.user}</h1>
-          <p className="description">{detail.userDescription}</p>
-        </div>
+      </div>
 
+      <div className="text-container">
+        <h1 className="title">{profile.name}</h1>
+        {profile.userType === 'customer' ? (
+          <>
+            <p className="description">Birth date: {birthDate}</p>
+            <p className="description">Email: {profile.email}</p>
+          </>
+        ) : profile.userType === 'company' ? (
+          <p className="description">{profile.bio}</p>
+        ) : null}
+      </div>
     </div>
   );
 };
--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -16,56 +16,64 @@ const UserProfile = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [cart, setCart] = useState([]);
+    const [profile, setProfile] = useState([]);
+
+    const fetchData = async () => {
+        if (!user) {
+            setCart([]);
+            localStorage.removeItem('cart');
+            setLoading(false);
+            return;
+        }
+
+        setLoading(true);
+        try {
+            const userId = user.user.id;
+            const token = user.token;
+
+            // Fetch user data
+            const userResponse = await axios.get(`http://127.0.0.1:3001/profile`, {
+                headers: { Authorization: `Bearer ${token}` }
+            });
+            setProfile(userResponse.data); 
+            console.log(userResponse.data);
+
+            // Fetch cart data
+            const cartResponse = await axios.post(`http://127.0.0.1:3001/carts`, { userId }, {
+                headers: { Authorization: `Bearer ${token}` },
+            });
+            setCart(cartResponse.data);
+
+            // Fetch game details
+            const gamesResponse = await axios.get('http://127.0.0.1:3001/games/');
+            setGames(gamesResponse.data);
+
+            // Fetch wishlist items
+            const wishlistResponse = await axios.get(`http://127.0.0.1:3001/wishlists/items/all`, {
+                headers: { Authorization: `Bearer ${token}` },
+            });
+            
+            setWishlistItems(wishlistResponse.data);
+        } catch (err) {
+            setError('Error fetching data');
+            console.error(err);
+        } finally {
+            setLoading(false);
+        }
+    };
+
 
     // Fetch data on component mount
     useEffect(() => {
-        const fetchData = async () => {
-            if (!user) {
-                setCart([]);
-                localStorage.removeItem('cart');
-                setLoading(false);
-                return;
-            }
-
-            setLoading(true);
-            try {
-                const userId = user.user.id;
-                const token = user.token;
-
-                // Fetch cart data
-                const cartResponse = await axios.post(`http://127.0.0.1:3001/carts/`, { userId }, {
-                    headers: { Authorization: `Bearer ${token}` },
-                });
-                setCart(cartResponse.data);
-
-                // Fetch game details
-                const gamesResponse = await axios.get('http://127.0.0.1:3001/games/');
-
-                setGames(gamesResponse.data);
-
-                // Fetch wishlist items
-                const wishlistResponse = await axios.get(`http://127.0.0.1:3001/wishlists/items/all`, {
-                    headers: { Authorization: `Bearer ${token}` },
-                });
-                console.log(wishlistResponse.data);
-                setWishlistItems(wishlistResponse.data);
-            } catch (err) {
-                setError('Error fetching data');
-                console.error(err);
-            } finally {
-                setLoading(false);
-            }
-        };
-
         fetchData();
     }, [user]);
 
-  
+
     return (
         <div className="user-profile">
-            {/* <UserCover  />    */}
+            <UserCover profile={profile} />
             {/* <ProductView  /> */}
-            <Wishlist itemGames={wishlistItems} games={games} />
+            {/* <Wishlist itemGames={wishlistItems} games={games} /> */}
         </div>
     );
 };
